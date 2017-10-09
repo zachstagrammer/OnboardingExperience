d4417b8a7a09b16b691e24a008723a46bfa553f0
Zach Phillips <zjohnny94@gmail.com>
2017-10-09 13:05:01 -0500

    Initial adding of files

---
+++ OnboardingExperience/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Principal;
+
+namespace OnboardingExperience
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User();
+
+            Console.WriteLine("Hello! Welcome to the Onboarding Experience");
+
+            user.FirstName = AskQuestion("What is your first name?");
+            Console.WriteLine("Awesome, your name is " + user.FirstName);
+
+            user.LastName = AskQuestion("What is your last name?");
+            Console.WriteLine("Excellent! Your full name is: " + user.FirstName + " " + user.LastName);
+
+            user.AType = AType("What type of account are you trying to access? Options: Checking, Savings, or Business");
+            Console.WriteLine("You are have requested access for the " + user.AType + " account");
+
+            user.IsAccountOwner = AskBoolQuestion("Are you the account owner? ");
+            Console.WriteLine("You are: " + user.IsAccountOwner);
+
+            user.PinNumber = AskPinNumber("What is your 4-digit pin number:", 4);
+            Console.WriteLine("Awesome! You entered: " + user.PinNumber);
+
+            Console.ReadLine();
+        }
+
+        static string AskQuestion(string question)
+        {
+            Console.WriteLine(question);
+            return Console.ReadLine();
+        }
+
+        static bool AskBoolQuestion(string question)
+        {
+            var isAccountOwner = false;
+
+            while (!isAccountOwner)
+            {
+                var response = AskQuestion(question + "(y/n)");
+                
+                if (response == "y")
+                {
+                    isAccountOwner = true;
+                }
+                else if (response == "n")
+                {
+                    Console.WriteLine("You must be the account owner to continue ");
+                }
+            }
+
+            return isAccountOwner;
+
+        }
+
+        public static AccountType AType(string question)
+        {
+            AccountType? aType = null;
+
+            while (aType == null)
+            {
+                var response = AskQuestion(question);
+
+                if (Enum.TryParse(response, out AccountType actualType))
+                {
+                    aType = actualType;
+                }
+                else
+                {
+                    Console.Write("Not a valid input. ");
+                }
+            }
+
+            return (AccountType)aType;
+
+        }
+
+        static string AskPinNumber(string question, int length)
+        {
+            string numberString = null;
+
+            while (numberString == null)
+            {
+                var response = AskQuestion(question);
+
+                if (response.Length == length && Int32.TryParse(response, out int _))
+                {
+                    numberString = response;
+                }
+            }
+
+            return numberString;
+
+        }
+    }
+}
---
+++ OnboardingExperience/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnboardingExperience
+
+{
+    public enum AccountType
+    {
+        Checking,
+        Savings,
+        Business
+    }
+
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsAccountOwner { get; set; }
+        public string PinNumber { get; set; }
+        public int AccountNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public AccountType AType { get; set; }
+    }
+}
